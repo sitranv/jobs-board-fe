cdbfae816201742c5501f84415eee9fffb8f70ef
sitranv <sitranhue@gmail.com>
2022-03-21 16:49:46 +0700

    change name

--- src/redux/services/rs/rs.service.ts
+++ src/redux/services/rs/rs.service.ts
@@ -8,7 +8,7 @@ export const getRS = async (data: any): Promise<RSResponse> => {
   return await axios.post(process.env.REACT_APP_SERVER_URL + `api/jobs/fast-apply/cv`, data, {
     headers: {
         'Content-Type': 'multipart/form-data',
-        'authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQGxvY2FsaG9zdC5jb20iLCJhdXRoIjoiUk9MRV9VU0VSIiwiZXhwIjoxNjQxNzE0NDM3fQ.9FwnrsboBT0B3CmvDYCnuHizSAy8rrhU2hR2j8xG0hw`
+        'authorization': `Bearer ${token}`
     }
   })
 }
